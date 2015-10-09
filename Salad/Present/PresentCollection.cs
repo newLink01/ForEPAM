85ca0d82fa8d5db80dc4b63f5c088a96a3e0687f
newLink01 <123456789newLink01>
2015-10-09 17:56:45 +0300

    PresentCollection

--- Salad/Present/PresentCollection.cs
+++ Salad/Present/PresentCollection.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Collections;
 namespace Present
 {
-    class PresentCollection
+    class PresentCollection<material> : ICollection<material> where material: ISweet                           
     {
+        private List<material> sweetColletion;
 
+        public PresentCollection() {
+            this.sweetColletion = new List<material>();
+        }
 
-
+        public int Count() {
+            return this.sweetColletion.Count;
+        }
 
 
     }
