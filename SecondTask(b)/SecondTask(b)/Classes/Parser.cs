89ca85412d0e74d28831b45f405ac34523974a92
newLink01 <123456789newLink01>
2015-10-31 00:58:24 +0300

    FirstPartComplete

---
+++ SecondTask(b)/SecondTask(b)/Classes/Parser.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using SecondTask_b_.Interfaces;
+namespace SecondTask_b_.Classes
+{
+   public class Parser
+    {
+       private StreamReader reader;
+       private Text textObject;
+       private Separators sep;
+
+       public Parser(string path) {
+           textObject = new Text();
+           sep = new Separators();
+           try
+           {
+               reader = new StreamReader(path);
+           }
+           catch (FileNotFoundException ex) {
+               Console.Write(ex.Message);
+           }
+           textObject = new Text();
+       }
+
+
+       private Line LineParse(string str) {
+           Line newLine = new Line();
+
+               string[] containsWords = str.Split(sep.AllSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+               for (int i = 0; i < containsWords.Length; i++)
+               {
+                   newLine.items.Add(new Word(containsWords[i].ToLower()));
+               }
+               return newLine;
+           
+       }
+
+     
+       public Text TextParse() {
+
+           string str = null;
+
+           while (true)
+           {
+               str = reader.ReadLine();
+               if (String.IsNullOrWhiteSpace(str)) { break; }
+               else
+               {
+                   textObject.text.Add(LineParse(str));
+               }
+           }
+
+
+           return textObject;
+       }
+
+
+
+
+
+    }
+}
---
+++ SecondTask(b)/SecondTask(b)/Interfaces/ILine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SecondTask_b_.Classes;
+namespace SecondTask_b_.Interfaces
+{
+  public interface ILine:IEnumerable<IWord>
+    {
+      List<IWord> items { set; get; }
+    }
+}
