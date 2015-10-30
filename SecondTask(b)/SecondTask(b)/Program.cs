95e6fcc44bbd4a7635bd1f963c1725adf6404b7e
newLink01 <123456789newLink01>
2015-10-30 14:14:47 +0300

    First

---
+++ SecondTask(b)/SecondTask(b)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondTask_b_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+        }
+    }
+}
