93bd5a2ffe1790c9561b5d4dd28da93872ccae1c
newLink01 <123456789newLink01>
2015-12-11 13:33:49 +0300

    BeginStatistic

--- Statistic/Statistic/Properties/AssemblyInfo.cs
+++ Statistic/Statistic/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FifthTask")]
+[assembly: AssemblyTitle("Statistic")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FifthTask")]
+[assembly: AssemblyProduct("Statistic")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("25b5922e-eb58-46a2-9a0b-dee5073dc268")]
+[assembly: Guid("b4320f88-1ffb-4ff3-94fa-b224bc63ffbb")]
 
 // Version information for an assembly consists of the following four values:
 //
