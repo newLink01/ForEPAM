5d77e9a262a69b7f3190ce04c3affba501a8c655
newLink01 <123456789newLink01>
2015-12-11 18:47:25 +0300

    AddRepositories

---
+++ FifthTask/FifthTask/DAL/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifthTask.DAL.Repositories
+{
+   public interface IRepository<T> : IDisposable where T:class
+    {
+        IEnumerable<T> GetAll();
+        T GetElement(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+        void Save();
+    }
+}
