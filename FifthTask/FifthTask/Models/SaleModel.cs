a6981223b750d2f491bacd240e0664a2b04ab385
newLink01 <123456789newLink01>
2015-12-13 15:43:17 +0300

    Refactor

--- FifthTask/FifthTask/Models/SaleModel.cs
+++ FifthTask/FifthTask/Models/SaleModel.cs
@@ -9,16 +9,25 @@ namespace FifthTask.Models
     public class SaleModel
     {
         [Required]
+        [Display(Name = "Manager id")]
         public int ManagerId { set; get; }
+
         [Required]
+        [Display(Name = "Product id")]
         public int ProductId { set; get; }
+
         [Required]
+        [Display(Name = "Amount for sale")]
         public int AmountForSale { set; get; }
+
         [Required]
+        [Display(Name = "Cost per unit")]
         public double CostPerUnit { set; get; }
+
         [Required]
         [DataType(DataType.Date)]
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        [Display(Name = "Date of sale")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:g}")]
         public DateTime DateOfSale { set; get; }
     }
 }
